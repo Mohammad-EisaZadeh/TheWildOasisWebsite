216b436cac402b5bd552dbfef283c3bbe9506840
KDlHidden <mohammad13820.mi@gmail.com>
2025-06-15 22:21:00 +0330

    fixing some bugs

--- app/account/page.js
+++ app/account/page.js
@@ -4,8 +4,6 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <h2 className="mb-4 text-2xl font-semibold text-accent-400">
-      Welcome Jonas
-    </h2>
+    <h2 className="mb-4 text-2xl font-semibold text-accent-400">Welcome</h2>
   );
 }
